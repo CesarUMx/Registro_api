e2eda12b56780a623289b1ae6d6a7a7553106d0f
CesarOrtiz1994 <cesarortize94@gmail.com>
2025-05-12 13:01:57 -0600

    iniciar

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('OK'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API corriendo en ${PORT}`));
