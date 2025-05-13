1a7d23dea92b6a98fbdff24b4fee590d311d8087
CesarOrtiz1994 <cesarortize94@gmail.com>
2025-05-13 11:48:38 -0600

    inicio del crud

--- src/index.js
+++ src/index.js
@@ -1,32 +1,39 @@
 // src/index.js
 require('dotenv').config();
 const express = require('express');
-const pool    = require('./config/db');  // <— importa el pool
 const app     = express();
 const authRouter = require('./routes/auth');
 const passport = require('./config/passport');
+const { verifyJWT, requireRole } = require('./middlewares/auth');
+const upload = require('./middlewares/upload');
+
 
 app.use(passport.initialize());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ ok: true, message: 'API corriendo correctamente' });
 });
 
-app.get('/db-health', async (req, res) => {
-  try {
-    const { rows } = await pool.query('SELECT NOW()');
-    res.json({ ok: true, now: rows[0].now });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ ok: false, error: 'DB connection error' });
-  }
-});
-
-app.use('/auth', express.json(), authRouter);
-
+app.use('/auth', authRouter);
 app.get('/login-failure', (req, res) => {
   res.status(401).json({ ok: false, error: 'Autenticación con Google fallida' });
 });
+
+//rutas protegidas
+
+//rutas de test
+app.get('/protected', verifyJWT, requireRole('admin','sysadmin'), (req, res) => {
+  res.json({ ok: true, message: 'Ruta protegida', user: req.user });
+});
+
+app.post('/upload', verifyJWT, upload.fields([
+  { name: 'idPhoto', maxCount: 1 },
+  { name: 'platePhoto', maxCount: 1 }
+]), (req, res) => {
+  res.json({ ok: true, message: 'Archivos subidos correctamente', files: req.files });
+});
+  
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
